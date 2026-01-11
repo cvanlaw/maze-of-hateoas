b772e21015b916a2fe094fe09e18f9e72e39a7a1
Chris Van Law <chris@chrisvanlaw.com>
2026-01-11 08:56:11 -0500

    feat: add shared TestRepositories for unit tests

---
+++ tests/MazeOfHateoas.UnitTests/Helpers/TestRepositories.cs
@@ -0,0 +1,43 @@
+using MazeOfHateoas.Application.Interfaces;
+using MazeOfHateoas.Domain;
+
+namespace MazeOfHateoas.UnitTests.Helpers;
+
+public class TestMazeRepository : IMazeRepository
+{
+    private readonly Dictionary<Guid, Maze> _mazes = new();
+
+    public Task<Maze?> GetByIdAsync(Guid id) =>
+        Task.FromResult(_mazes.GetValueOrDefault(id));
+
+    public Task<IEnumerable<Maze>> GetAllAsync() =>
+        Task.FromResult<IEnumerable<Maze>>(_mazes.Values.ToList());
+
+    public Task SaveAsync(Maze maze)
+    {
+        _mazes[maze.Id] = maze;
+        return Task.CompletedTask;
+    }
+
+    public void Add(Maze maze) => _mazes[maze.Id] = maze;
+}
+
+public class TestSessionRepository : ISessionRepository
+{
+    private readonly Dictionary<Guid, MazeSession> _sessions = new();
+
+    public Task<MazeSession?> GetByIdAsync(Guid id) =>
+        Task.FromResult(_sessions.GetValueOrDefault(id));
+
+    public Task<IEnumerable<MazeSession>> GetByMazeIdAsync(Guid mazeId) =>
+        Task.FromResult<IEnumerable<MazeSession>>(
+            _sessions.Values.Where(s => s.MazeId == mazeId).ToList());
+
+    public Task SaveAsync(MazeSession session)
+    {
+        _sessions[session.Id] = session;
+        return Task.CompletedTask;
+    }
+
+    public void Add(MazeSession session) => _sessions[session.Id] = session;
+}
