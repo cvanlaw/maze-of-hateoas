d4ddd508fafacf23e66a7489902256034bcb0550
Chris Van Law <chris@chrisvanlaw.com>
2026-01-11 09:00:30 -0500

    refactor: use shared test helpers in SessionsControllerLoggingTests
    - Replaced duplicate TestMazeRepository and TestSessionRepository
      with shared implementations from UnitTests.Helpers
    - Replaced local CreateTestMaze/CreateTestMazeWithOpenPaths with
      TestDataBuilders.CreateTestMaze()
    - Replaced direct MazeSession construction with
      TestDataBuilders.CreateTestSession()
    - Kept CreateMazeForCompletion() as it's a unique 2x1 maze for testing

--- tests/MazeOfHateoas.UnitTests/Controllers/SessionsControllerLoggingTests.cs
+++ tests/MazeOfHateoas.UnitTests/Controllers/SessionsControllerLoggingTests.cs
@@ -1,8 +1,7 @@
 using MazeOfHateoas.Api.Controllers;
-using MazeOfHateoas.Api.Models;
 using MazeOfHateoas.Api.Services;
-using MazeOfHateoas.Application.Interfaces;
 using MazeOfHateoas.Domain;
+using MazeOfHateoas.UnitTests.Helpers;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
 
@@ -27,7 +26,7 @@ public class SessionsControllerLoggingTests
     [Fact]
     public async Task CreateSession_LogsInformationWithSessionDetails()
     {
-        var maze = CreateTestMaze();
+        var maze = TestDataBuilders.CreateTestMaze(5, 5, allCellsOpen: false);
         await _mazeRepository.SaveAsync(maze);
 
         await _controller.CreateSession(maze.Id);
@@ -40,7 +39,7 @@ public class SessionsControllerLoggingTests
     [Fact]
     public async Task GetSession_WhenSessionNotFound_LogsWarning()
     {
-        var maze = CreateTestMaze();
+        var maze = TestDataBuilders.CreateTestMaze(5, 5, allCellsOpen: false);
         await _mazeRepository.SaveAsync(maze);
 
         await _controller.GetSession(maze.Id, Guid.NewGuid());
@@ -53,9 +52,9 @@ public class SessionsControllerLoggingTests
     [Fact]
     public async Task Move_WhenSuccessful_LogsInformation()
     {
-        var maze = CreateTestMazeWithOpenPaths();
+        var maze = TestDataBuilders.CreateTestMaze(5, 5, allCellsOpen: true);
         await _mazeRepository.SaveAsync(maze);
-        var session = new MazeSession(Guid.NewGuid(), maze.Id, maze.Start);
+        var session = TestDataBuilders.CreateTestSession(maze.Id, maze.Start);
         await _sessionRepository.SaveAsync(session);
 
         await _controller.Move(maze.Id, session.Id, "south");
@@ -70,7 +69,7 @@ public class SessionsControllerLoggingTests
     {
         var maze = CreateMazeForCompletion();
         await _mazeRepository.SaveAsync(maze);
-        var session = new MazeSession(Guid.NewGuid(), maze.Id, maze.Start);
+        var session = TestDataBuilders.CreateTestSession(maze.Id, maze.Start);
         await _sessionRepository.SaveAsync(session);
 
         await _controller.Move(maze.Id, session.Id, "east");
@@ -80,71 +79,11 @@ public class SessionsControllerLoggingTests
         Assert.Contains(logs, l => l.Message.Contains("completed"));
     }
 
-    private static Maze CreateTestMaze()
-    {
-        var cells = new Cell[5, 5];
-        for (int x = 0; x < 5; x++)
-        {
-            for (int y = 0; y < 5; y++)
-            {
-                cells[x, y] = new Cell(new Position(x, y), true, true, true, true);
-            }
-        }
-        return new Maze(Guid.NewGuid(), 5, 5, cells, new Position(0, 0), new Position(4, 4), DateTime.UtcNow);
-    }
-
-    private static Maze CreateTestMazeWithOpenPaths()
-    {
-        var cells = new Cell[5, 5];
-        for (int x = 0; x < 5; x++)
-        {
-            for (int y = 0; y < 5; y++)
-            {
-                cells[x, y] = new Cell(new Position(x, y), false, false, false, false);
-            }
-        }
-        return new Maze(Guid.NewGuid(), 5, 5, cells, new Position(0, 0), new Position(4, 4), DateTime.UtcNow);
-    }
-
     private static Maze CreateMazeForCompletion()
     {
         var cells = new Cell[2, 1];
         cells[0, 0] = new Cell(new Position(0, 0), true, true, false, true);
         cells[1, 0] = new Cell(new Position(1, 0), true, true, true, false);
         return new Maze(Guid.NewGuid(), 2, 1, cells, new Position(0, 0), new Position(1, 0), DateTime.UtcNow);
-    }
-
-    private class TestMazeRepository : IMazeRepository
-    {
-        private readonly Dictionary<Guid, Maze> _mazes = new();
-
-        public Task<Maze?> GetByIdAsync(Guid id) =>
-            Task.FromResult(_mazes.GetValueOrDefault(id));
-
-        public Task<IEnumerable<Maze>> GetAllAsync() =>
-            Task.FromResult<IEnumerable<Maze>>(_mazes.Values);
-
-        public Task SaveAsync(Maze maze)
-        {
-            _mazes[maze.Id] = maze;
-            return Task.CompletedTask;
-        }
-    }
-
-    private class TestSessionRepository : ISessionRepository
-    {
-        private readonly Dictionary<Guid, MazeSession> _sessions = new();
-
-        public Task<MazeSession?> GetByIdAsync(Guid id) =>
-            Task.FromResult(_sessions.GetValueOrDefault(id));
-
-        public Task<IEnumerable<MazeSession>> GetByMazeIdAsync(Guid mazeId) =>
-            Task.FromResult(_sessions.Values.Where(s => s.MazeId == mazeId));
-
-        public Task SaveAsync(MazeSession session)
-        {
-            _sessions[session.Id] = session;
-            return Task.CompletedTask;
-        }
     }
 }
