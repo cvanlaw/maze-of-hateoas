ccf7b9b2170d8db6b045cdb3715a2d483a357675
Chris Van Law <chris@chrisvanlaw.com>
2026-01-12 10:53:03 -0500

    feat(solver): add SolverSettings configuration class

---
+++ src/MazeOfHateoas.Solver/Configuration/SolverSettings.cs
@@ -0,0 +1,13 @@
+namespace MazeOfHateoas.Solver.Configuration;
+
+public class SolverSettings
+{
+    public const string SectionName = "Solver";
+
+    public string ApiBaseUrl { get; set; } = "http://localhost:8080";
+    public int MazeWidth { get; set; } = 10;
+    public int MazeHeight { get; set; } = 10;
+    public int DelayBetweenMazesMs { get; set; } = 2000;
+    public int DelayBetweenMovesMs { get; set; } = 0;
+    public int StatsIntervalMazes { get; set; } = 10;
+}
